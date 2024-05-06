ac130ca5474797eabc99fc05b40c2c674aac0195
sergii13 <sergii.khilchuk13@gmail.com>
2024-05-06 17:07:52 +0300

    fix scroll

--- src/js/main.js
+++ src/js/main.js
@@ -3,16 +3,14 @@ import "./files/script.js";
 import * as Functions from "./files/functions.js";
 import "./files/sliders.js";
 import { MousePRLX } from "./libs/parallax-mouse.js";
-//svg-sprite
+
 import "virtual:svg-icons-register";
 import { Parallax } from "./libs/parallax.js";
 
 Functions.menuInit();
-// Functions.tabs();
-// Functions.spollers();
+
 if (document.querySelectorAll("[data-prlx-parent]")) {
   new Parallax(document.querySelectorAll("[data-prlx-parent]"));
 }
-// const popupInstance = new Popup({});
-// new SelectConstructor({});
+
 new MousePRLX({});
